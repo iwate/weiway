0aa3cb8ed99af409cb1e1013890a9cf20d8b48f1
iwate <iwatesotware@live.jp>
2014-04-29 06:42:05 +0900

    create project

---
+++ WeiWay/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WeiWay.Startup))]
+namespace WeiWay
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
