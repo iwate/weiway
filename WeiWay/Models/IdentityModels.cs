a6a75370b284447da759388c52d831dbaccc8401
iwate <iwatesotware@live.jp>
2014-04-29 07:59:40 +0900

    change  user secret information

--- WeiWay/Models/IdentityModels.cs
+++ WeiWay/Models/IdentityModels.cs
@@ -3,12 +3,37 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.Data.Entity;
+using Newtonsoft.Json;
 
 namespace WeiWay.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        [JsonIgnore]
+        public override string PasswordHash
+        {
+            get
+            {
+                return base.PasswordHash;
+            }
+            set
+            {
+                base.PasswordHash = value;
+            }
+        }
+        [JsonIgnore]
+        public override string SecurityStamp
+        {
+            get
+            {
+                return base.SecurityStamp;
+            }
+            set
+            {
+                base.SecurityStamp = value;
+            }
+        }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
