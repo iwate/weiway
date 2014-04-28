a9f2098bf0657998db8883f6a34c35f310c0a255
iwate <iwatesotware@live.jp>
2014-04-29 06:56:48 +0900

    create model that is base model for all models

---
+++ WeiWay/Models/BaseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeiWay.Models
+{
+    public class BaseModel
+    {
+        public string Id { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+        public BaseModel()
+        {
+            Id = Guid.NewGuid().ToString();
+            CreateTime = DateTime.Now;
+            UpdateTime = DateTime.Now;
+        }
+    }
+}
