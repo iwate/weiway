688aad7d54a9aae88f8a766d158efc2dbfe96ae5
iwate <iwatesotware@live.jp>
2014-04-29 07:53:46 +0900

    create wei model

---
+++ WeiWay/Models/Wei.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeiWay.Models
+{
+    public class Wei : BaseModel
+    {
+        public string MessageId { get; set; }
+        public virtual Message Message { get; set; }
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
